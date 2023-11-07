0da4fb08bc3a4d1eedb036c760484020c1634df7
saisunil <saisunilmukkanti@gmail.com>
2023-11-07 13:00:23 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, {useEffect, useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const [users,setUsers] = useState([]) 
+
+  useEffect( () => {
+    axios("https://jsonplaceholder.typicode.com/users")
+    .then( (res) =>{
+      setUsers(res.data)
+    })
+    .catch((error) => {
+      console.error('Error fetching data: ', error);
+    });
+  },[])
+
+  return(
+    <>
+    <h2> Users Data </h2>
+    <ul>
+    {users.map((user,i) => {
+      // console.log(user.name)
+      return <li key={i}> { user.name }</li>
+    })}
+
+    </ul>
+    </>
+  )
 }
 
 export default App;
