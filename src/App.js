e07dfc6502c6aa25ead0baf818c69ae99e4af301
saisunil <saisunilmukkanti@gmail.com>
2023-11-18 10:53:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,64 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
-import "./App.css";
-import MovieCards from './MovieCards';
+import React, { useState } from 'react'
+import "./App.css"
 
-function App() {
+function SetTimer() {
 
-  const [popularMovies, setPopularMovies] = useState([])
-  const [movieName, setMovieName] = useState("")
-  const [searchedMovies, setSearchedMovies] = useState([])
+  const [timer, setTimer] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
 
-  useEffect(() => {
-    axios.get("https://api.themoviedb.org/3/movie/popular?api_key=cfe422613b250f702980a3bbf9e90716")
-      .then((res) => {
-        setPopularMovies(res.data.results)
-      }).catch((err) => {
-        console.log(err);
-      })
-  }, [])
+  const startTimer = () => {
+    if (!intervalId) {
+      const id = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 1);
+      }, 1000);
+      setIntervalId(id);
+    }
+  };
 
-  useEffect(() => {
-    axios.get(`https://api.themoviedb.org/3/search/movie?query=${movieName}&api_key=cfe422613b250f702980a3bbf9e90716`)
-      .then((res) => {
-        setSearchedMovies(res.data.results)
-      }).catch((err) => {
-        console.log(err);
-      })
-  }, [movieName])
+  const stopTimer = () => {
+    clearInterval(intervalId);
+    setIntervalId(null);
+  };
+
+  const resetTimer = () => {
+    clearInterval(intervalId);
+    setIntervalId(null);
+    setTimer(0);
+  };
 
   return (
     <>
-      <div>
-        <h1> Movies Results </h1>
-        <input id="inputValue"
-          placeholder='Search Movies'
-          value={movieName}
-          onChange={(e) => { setMovieName(e.target.value) }}
-        />
-      </div>
-      <div id='movie-wrapper'>
-        {movieName == "" ? popularMovies.map((item, i) => {
-          return (
-            <MovieCards 
-            poster = {item.poster_path}
-            title = {item.title}
-            Rating = {item.vote_average}
-            ReleaseDate = {item.release_date}
-            />
-            )
-        }) : searchedMovies.map((item, i) => {
-          return (
-            <MovieCards 
-            poster = {item.poster_path}
-            title = {item.title}
-            Rating = {item.vote_average}
-            ReleaseDate = {item.release_date}
-            />)
-        })}
+      <div id='timer'>
+        <h1> Setting Timer Interval </h1>
+        <h4>Timer: {timer}</h4>
+        <div id='b1'>
+          <button id='b11' onClick={startTimer} disabled={!!intervalId}> Start </button>
+          <button onClick={stopTimer}>Stop</button>
+          <button onClick={resetTimer}>Reset</button>
+        </div>
       </div>
     </>
   )
 }
-
-export default App
+export default SetTimer;
